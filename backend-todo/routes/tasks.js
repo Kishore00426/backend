1b091670f5ccb81c76cfdb0a7662e154f3b95535
Kishore00426 <kishorefsdeveloper@gmail.com>
2025-11-07 15:51:53 +0530

    status changed to true

--- backend-todo/routes/tasks.js
+++ backend-todo/routes/tasks.js
@@ -8,7 +8,7 @@ router.post("/", (req, res) => {
   const newTask = {
     id: tasks.length + 1,
     title: req.body.title,
-    completed: false
+    completed: true
   };
   tasks.push(newTask);
   res.status(201).json(newTask);
